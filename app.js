c615882e27c5d4a2b4524f358795d0f310ba5ddc
jacoder222 <bjuop222@gmail.com>
2024-01-31 15:15:15 +0900

    File structure and markdown writing

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-aa;
+import express from "express";
+import UsersRouter from "../routes/user.router.js";
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use("/api", [UsersRouter]);
+
+app.listen(PORT, () => {
+  console.log(PORT, "포트로 서버가 열렸어요!");
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+### API 설계
+
+[API노션페이지](https://gainful-bonsai-fe1.notion.site/API-933152851dff4395a74d931328dd5429?pvs=4)
+
+### ERD 설계 링크
+
+![텍스트](img/ERD.png)
+
+[ERD링크](https://www.erdcloud.com/p/6pgLDQpbf6z2KhtPK)
+
+#
+
+API설계와 ERD는 초안이므로 수정되거나 삭제될 수 있습니다.
